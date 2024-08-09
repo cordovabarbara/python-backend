a853cf9a10a68a8326624b87f0f1d874925357a7
cordovabarbara <aliendocordova@gmail.com>
2024-08-08 19:11:10 -0500

    mas ejemplos del uso de los hints

--- Backend/type_hints.py
+++ Backend/type_hints.py
@@ -12,5 +12,9 @@ def get_full_name(first_name: str, last_name: str):
     full_name = first_name.title() + " " + last_name.title()
     return full_name
 
+print(get_full_name("barbara", "cordova"))
 
-print(get_full_name("john", "doe"))
+def get_name_with_age(name: str, age: int):
+    name_with_age = name.upper() + " is this old: " + str(age)
+    return name_with_age
+print (get_name_with_age("barbara", 32))

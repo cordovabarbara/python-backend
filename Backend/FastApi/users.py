a3801ae3482fe0706b7224970fa93f5c2dd9e373
cordovabarbara <aliendocordova@gmail.com>
2024-08-26 10:00:38 -0500

    aplicar Basemodal, crear clase user

--- Backend/FastApi/users.py
+++ Backend/FastApi/users.py
@@ -5,14 +5,15 @@ app = FastAPI()
 
 #Entidad User
 class User(BaseModel):
+    id: int
     name: str
     lastname: str
     url: str
     age: int
 
-users_list = [User(name = "Barbara", lastname ="Cordova", url = "https://barbara-cordova-portfolio.netlify.app/", age = 32),
-              User(name = "Anais", lastname ="Aliendo",  url = "https://barbara-cordova-portfolio.netlify.app/", age = 35),
-              User(name = "Cristobal", lastname ="Lopez", url = "https://barbara-cordova-portfolio.netlify.app/", age = 30)]
+users_list = [User(id= 1, name = "Barbara", lastname ="Cordova", url = "https://barbara-cordova-portfolio.netlify.app/", age = 32),
+              User(id= 2, name = "Anais", lastname ="Aliendo",  url = "https://barbara-cordova-portfolio.netlify.app/", age = 35),
+              User(id= 3,name = "Cristobal", lastname ="Lopez", url = "https://barbara-cordova-portfolio.netlify.app/", age = 30)]
 
 @app.get("/users")
 def read_users():
